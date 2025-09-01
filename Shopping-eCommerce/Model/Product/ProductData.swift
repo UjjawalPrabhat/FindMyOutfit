166a54b142d00339478b29858102825ed72834ee
Raj-bytecommandor <rjnt452@gmail.com>
2025-09-01 23:23:14 +0530

    add ProductData and ProductModel structs for product management

---
+++ Shopping-eCommerce/Model/Product/ProductData.swift
@@ -0,0 +1,24 @@
+//
+//  ProductData.swift
+//  FindMyOutfit
+//
+//  
+//
+
+import Foundation
+
+struct ProductData: Codable {
+    let id: Int
+    let title: String
+    let price: Double
+    let description: String
+    let image: String
+    let category: String
+    let rating: Rating
+}
+
+struct Rating: Codable {
+    let rate: Double
+    let count: Int
+}
+
---
+++ Shopping-eCommerce/Model/Product/ProductModel.swift
@@ -0,0 +1,20 @@
+//
+//  ProductModel.swift
+//  FindMyOutfit
+//
+//  
+//
+
+import Foundation
+
+struct ProductModel {
+    var id: Int?
+    var title: String?
+    var price: Float?
+    var image: String?
+    var rate: Float?
+    var category: String?
+    var description: String?
+    var count: Int?
+    var quantityCount: Int?
+}
