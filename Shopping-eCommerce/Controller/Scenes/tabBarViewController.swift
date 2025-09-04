acc8ed4192470af1e0076c254ca28f4d63e6a9f7
Raj-bytecommandor <rjnt452@gmail.com>
2025-09-04 15:48:45 +0530

    add ViewController and TabBarViewController with initial setup and UI components

---
+++ Shopping-eCommerce/Controller/Scenes/tabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarViewController.swift
+//  FindMyOutfit
+//
+//  
+//
+
+import UIKit
+
+class TabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.setHidesBackButton(true, animated: true)
+        // Do any additional setup after loading the view.
+    }
+}
