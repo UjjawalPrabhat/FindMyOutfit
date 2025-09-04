89e846dce4ddfacb8b4584d0fcec4be8ac4d4d42
UjjawalPrabhat <ujjawalprabhat1@gmail.com>
2025-09-04 23:13:01 +0530

    add CartViewController and CategorizedViewController with associated UI components and functionality

---
+++ Shopping-eCommerce/Controller/Scenes/Categorized/CategorizedTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategorizedTableViewCell.swift
+//  FindMyOutfit
+//
+//  
+//
+
+import UIKit
+
+class CategorizedTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productDescriptionLabel: UILabel!
+    @IBOutlet weak var productRateLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        // Configure the view for the selected state
+    }
+}
