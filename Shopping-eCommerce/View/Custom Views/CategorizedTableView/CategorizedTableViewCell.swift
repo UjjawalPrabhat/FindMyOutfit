d795c4f627df624c587b36ac32bd7d1df7fd4d05
UjjawalPrabhat <ujjawalprabhat1@gmail.com>
2025-09-06 22:03:58 +0530

    Add CategorizedTableViewCell implementation with XIB layout

---
+++ Shopping-eCommerce/View/Custom Views/CategorizedTableView/CategorizedTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CategorizedTableViewCell.swift
+//  Shopping-App-eCommerce
+//
+//  
+//
+
+import UIKit
+
+class CategorizedTableViewCell: UITableViewCell {
+
+    //MARK: - Properties
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productDescriptionLabel: UILabel!
+    @IBOutlet weak var productRateLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    
+}
