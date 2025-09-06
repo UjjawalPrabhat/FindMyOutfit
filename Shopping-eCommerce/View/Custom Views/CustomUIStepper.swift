40483a89f050ace84ebecf582720b22e308a801a
Raj-bytecommandor <rjnt452@gmail.com>
2025-09-06 22:06:22 +0530

    Add CustomUIStepper class implementation

---
+++ Shopping-eCommerce/View/Custom Views/CustomUIStepper.swift
@@ -0,0 +1,19 @@
+//
+//  CustomUIStepper.swift
+//  FindMyOutfit
+//
+//  
+//
+
+import UIKit
+
+class CustomUIStepper: UIStepper {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+}
